5464bf91fcd0e245adf6dbb12095c19814305255
gumio <quran.gumi3134@gmail.com>
2016-09-27 20:45:09 +0900

    formを再利用できるようにパーシャル化

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root'top_pages#home'
+  root 'top_pages#home'
   get 'about' => 'top_pages#about'
   get 'help' => 'top_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
