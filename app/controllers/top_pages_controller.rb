9e26ee9b4c1be4f1af02f11dde36125bc96d47a1
gumio <quran.gumi3134@gmail.com>
2016-09-27 15:09:38 +0900

    各ルーティングとリンクを設置 #2

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,4 +1,21 @@
 class TopPagesController < ApplicationController
+
   def home
   end
+
+  def search
+  end
+
+  def prefecture
+  end
+
+  def rank
+  end
+
+  def help
+  end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root'top_pages#home'
-
+  get 'search' => 'top_pages#search'
+  get 'prefecture' => 'top_pages#prefecture'
+  get 'rank' => 'top_pages#rank'
+  get 'about' => 'top_pages#about'
+  get 'help' => 'top_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
