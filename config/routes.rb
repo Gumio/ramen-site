40dcee5c34b5656113e42f5381b9945b5d03fa15
ebiryu <ebiryu.zatta@gmail.com>
2016-09-27 16:57:31 +0900

    route shops #6

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root'top_pages#home'
   get 'about' => 'top_pages#about'
   get 'help' => 'top_pages#help'
-  get 'shops/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
