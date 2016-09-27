9c5f463266c05fe2891c519dea16895769c72204
ebiryu <ebiryu.zatta@gmail.com>
2016-09-27 17:18:15 +0900

    route search shops #6

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root'top_pages#home'
   get 'about' => 'top_pages#about'
   get 'help' => 'top_pages#help'
+  get 'search'    => 'shops#index'
+  get 'rank'      => 'shops#ranking'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
