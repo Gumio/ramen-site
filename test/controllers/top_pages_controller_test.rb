371cf02b2f1a60f3ec9bd6e79a66ae2781dce77b
gumio <quran.gumi3134@gmail.com>
2016-09-27 15:18:53 +0900

    各タイトルを引数で設定されるようにした

--- test/controllers/top_pages_controller_test.rb
+++ test/controllers/top_pages_controller_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class TopPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get top_pages_home_url
+    get root_path
+    assert_response :success
+  end
+
+  test "should get search" do
+    get search_path
+    assert_response :success
+  end
+
+  test "should get prefecture" do
+    get prefecture_path
+    assert_response :success
+  end
+
+  test "should get rank" do
+    get rank_path
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+  end
+
+  test "should get help" do
+    get root_path
     assert_response :success
   end
 
