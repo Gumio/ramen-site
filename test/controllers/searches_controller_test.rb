a94d6d734ee97b088085ebb002b899d97ac55a4c
ebiryu <ebiryu.zatta@gmail.com>
2016-09-27 13:55:43 +0900

    added response test

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SearchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get search" do
+    get search_path
+    assert_response :success
+  end
 end
