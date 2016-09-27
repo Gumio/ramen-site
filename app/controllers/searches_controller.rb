57379abd6fa9e7a59097298c17345828233b566e
ebiryu <ebiryu.zatta@gmail.com>
2016-09-27 11:29:33 +0900

    show shops address in 'search' #3

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
 
   def home
-    @shops = ["一番星", "一二三", "一閃閣"]
+    @shops = [{name: "一番星", address: "仙台市"}, {name: "一二三", address: "仙台市"}, {name: "一閃閣", address: "仙台市"}]
   end
 end
