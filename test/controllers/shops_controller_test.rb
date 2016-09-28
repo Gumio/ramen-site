81b4799d9ac3df697f1d14779af218f19b97ccb9
ebiryu <ebiryu.zatta@gmail.com>
2016-09-28 12:55:50 +0900

    fix test parameter #27 #31

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -9,7 +9,7 @@ class ShopsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should sanitize search_word" do
-    post search_path, params: {q: "%' union select * from shops; --"}
+    post search_path, params: {q: "%') union select 100 as id, 'test' as name, 2 as prefecture_id, 'test' as address, '0' as tel, 'a' as about, '00:00' as open_time, '24:00' as close_time, 'test' as holiday, 0 as evaluation, 'a' as image, datetime('now') as created_at, datetime('now') as updated_at; --"}
     assert_response :success
     assert_select "li", 5
   end
