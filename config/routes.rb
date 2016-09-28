da4893b53f1651372ca437206544b1629fa4b2e3
gumio <quran.gumi3134@gmail.com>
2016-09-28 19:00:34 +0900

    idを引き渡して、表示するルーティング、showルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'prefectures/show'
 
   root 'top_pages#home'
   get 'about' => 'top_pages#about'
   get 'help' => 'top_pages#help'
   post 'search'   => 'shops#index'
   get  'search'   => 'shops#index'
+  get 'prefectures' => 'prefectures#index'
+  post 'prefectures' => 'prefecture#index'
+  get 'prefectures/:id' => 'prefectures#show', :as => :prefecture 
   get 'rank'      => 'shops#ranking'
-  get 'shop/:id'  => 'shops#show', :as => :shop
+  get 'shops/:id'  => 'shops#show', :as => :shop
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
