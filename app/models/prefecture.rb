f9b78ecf2b595a8b55b02e002598cd7a20c1fe59
gumio <quran.gumi3134@gmail.com>
2016-09-28 15:41:19 +0900

    各都道府県を識別するための定数IDとscopeメソッドを定義

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,4 +1,26 @@
 class Prefecture < ApplicationRecord
   has_many :shops
   validates(:name, presence: true)
+
+  HOKKAIDO_IDS = 1
+  TOHOKU_IDS = [2..7]
+  KANTOU_IDS = [8..14]
+  HOKURIKU_IDS = [15..18]
+  CHUBU_IDS = [19..24]
+  KANSAI_IDS = [25..30]
+  CHUGOKU_IDS = [31..35]
+  SHIKOKU_IDS = [36..39]
+  KYUSHU_IDS = [40..46]
+  OKINAWA_IDS = 47
+
+  scope :hokkaido, -> { where(id: HOKKAIDO_IDS) }
+  scope :tohoku,   -> { where(id: TOHOKU_IDS) }
+  scope :kantou,   -> { where(id: KANSAI_IDS) }
+  scope :hokuriku, -> { where(id: HOKURIKU_IDS) }
+  scope :chubu,    -> { where(id: CHUBU_IDS) }
+  scope :kansai,   -> { where(id: KANSAI_IDS) }
+  scope :chugoku,  -> { where(id: CHUGOKU_IDS) }
+  scope :shikoku,  -> { where(id: SHIKOKU_IDS) }
+  scope :kyushu,   -> { where(id: KYUSHU_IDS) }
+  scope :okinawa,  -> { where(id: OKINAWA_IDS) }
 end
