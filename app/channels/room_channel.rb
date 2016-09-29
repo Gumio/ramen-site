a793e5018d06694e8c941a0f8ecd147540280429
gumio <quran.gumi3134@gmail.com>
2016-09-29 18:13:47 +0900

    クライアントから受け取った、メッセージをmessageモデルに追加するspeakを定義

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,13 +2,15 @@
 #サーバーサイド側
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  #受け取ったメッセージをMessageモデルに保存
+  def speak(data)
+    Message.create! content: data['message']
   end
 end
