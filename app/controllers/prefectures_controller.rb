d684496a6fcbc156692bc033fc55a9a8c25b1668
gumio <quran.gumi3134@gmail.com>
2016-09-29 11:08:39 +0900

    will_paginateを適用させた

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,15 +1,11 @@
 class PrefecturesController < ApplicationController
-
+  before_action :create_area, only: [:index]
   def index
   end
 
   def show
-    @shops = Shop.where(prefecture_id: params[:id])
+    @shops = Shop.where(prefecture_id: params[:id]).paginate(page: params[:page])
     @prefecture = Prefecture.find(params[:id]).name
   end
 
-  private
-    def index_params
-      params.permit(:q)
-    end
 end
