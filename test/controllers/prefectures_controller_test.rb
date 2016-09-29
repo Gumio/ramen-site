8de4c255f96ace905873ad371ecaedae5a7e821b
gumio <quran.gumi3134@gmail.com>
2016-09-29 11:17:08 +0900

    リンク先が表示されるかのテスト #37

--- test/controllers/prefectures_controller_test.rb
+++ test/controllers/prefectures_controller_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class PrefecturesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get prefectures_show_url
+    get prefectures_path
     assert_response :success
+  end
+
+  test "各都道府県の検索が表示されるかどうか" do
+    for i in 1..47 do
+      get prefectures_path"/#{i}"
+      assert_response :success
+    end
   end
 
 end
