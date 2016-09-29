8f5c82d41b57bcf198170cd702fcad6402b951c2
gumio <quran.gumi3134@gmail.com>
2016-09-29 11:06:16 +0900

    使っていないルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root 'top_pages#home'
   get 'about' => 'top_pages#about'
   get 'help' => 'top_pages#help'
-  post 'search'   => 'shops#index'
-  get  'search'   => 'shops#index'
+  get 'search'   => 'shops#index'
   get 'prefectures' => 'prefectures#index'
-  post 'prefectures' => 'prefecture#index'
-  get 'prefectures/:id' => 'prefectures#show', :as => :prefecture 
+  get 'prefectures/:id' => 'prefectures#show', :as => :prefecture
   get 'rank'      => 'shops#ranking'
   get 'shops/:id'  => 'shops#show', :as => :shop
 
