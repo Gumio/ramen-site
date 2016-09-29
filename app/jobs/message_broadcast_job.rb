9a70c033cd83d8aea2351e85c4c55bd93e5ce5f9
gumio <quran.gumi3134@gmail.com>
2016-09-29 19:02:25 +0900

    メッセージをクライアントにブロードキャストするためのperfomeを定義

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,18 @@
+#サーバーサイド側で登録されたコメントをクライアントにブロードキャスト
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  #ここでブロードキャストを実行
+  def perform(message)
+    ActionCable.server.broadcast 'room_channel',
+                                    message: render_message(message)
   end
+
+  #コントローラ以外の場所でビューをレンダリングする
+  private
+    def render_message(message)
+      ApplicationController.renderer.render(
+                                    partial: 'messages/message',
+                                      locals: { message: message })
+    end
 end
